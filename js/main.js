ec93a86d86d209c10442b13f232724f6f444018b
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-06-17 10:03:40 -0700

    Make button appear only when there is text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-
+// initial putting elements into variables
 const button = document.querySelector('button');  // get the button
 const img = document.querySelector('img'); // get the image
+const question = document.getElementById('question'); // get the input question field
+
+// start with Ask button hidden
+button.style.display = 'none';
+
+// check when keys are pushed
+question.addEventListener('keyup', function() {
+    if(question.value) {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+})
 
 button.addEventListener('click', function() {  // check when button is clicked
     ask();
